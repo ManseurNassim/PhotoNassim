4c936add817fcdc8334fcb0c0c0278593036f1f7
NASSIMOU 60660 <nassimmanseur1@gmail.com>
2024-09-19 23:19:52 +0200

    ajout de google analytic

--- src/main.js
+++ src/main.js
@@ -3,5 +3,20 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { createGtag } from 'vue-gtag-next';
 
-createApp(App).use(router).use(store).mount('#app');
+const app = createApp(App);
+
+// Utilise Vue Router et Vuex
+app.use(router);
+app.use(store);
+
+// Configuration de Google Analytics avec vue-gtag-next
+app.use(createGtag({
+  property: {
+    id: 'G-HTFKQ319ZT'  // Remplace par ton ID de suivi Google Analytics
+  }
+}));
+
+// Monte l'application sur l'élément avec l'ID "app"
+app.mount('#app');
