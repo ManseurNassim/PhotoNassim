d9b5e6e7816076f8dd7e0d421aa9336068fc2a9e
NASSIMOU 60660 <nassimmanseur1@gmail.com>
2024-09-19 23:24:23 +0200

    correction utilisation de google analytic

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-// src/main.js
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import { createGtag } from 'vue-gtag-next';
+import VueGtag from 'vue-gtag-next'; // Import du plugin correct
 
 const app = createApp(App);
 
@@ -12,11 +11,11 @@ app.use(router);
 app.use(store);
 
 // Configuration de Google Analytics avec vue-gtag-next
-app.use(createGtag({
+app.use(VueGtag, {
   property: {
     id: 'G-HTFKQ319ZT'  // Remplace par ton ID de suivi Google Analytics
   }
-}));
+});
 
 // Monte l'application sur l'élément avec l'ID "app"
 app.mount('#app');
