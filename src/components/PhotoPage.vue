408882e0b17e9911ba2f99a71b259a41c31017ee
NASSIMOU 60660 <nassimmanseur1@gmail.com>
2024-09-25 23:57:34 +0200

    modif texte recherche

--- src/components/PhotoPage.vue
+++ src/components/PhotoPage.vue
@@ -3,7 +3,7 @@
     <input
       type="text"
       v-model="searchQuery"
-      placeholder="Rechercher par mots-clés..."
+      placeholder="Rechercher par mots-clés, essayer paysage, architecture, nuit ..."
       class="search-bar"
     />
     <div class="photos-grid">
